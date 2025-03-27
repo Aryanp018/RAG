0ed80b37a5f3028101f767238970fd96dab193ba
Aryan Patodiya <68765895+Aryan-88@users.noreply.github.com>
2025-03-27 11:50:54 -0700

    dataset converting to vector

---
+++ dataset.py
@@ -0,0 +1,15 @@
+import ollama
+
+dataset = []
+with open('cat-facts.txt', 'r') as file:
+    dataset = file.readlines()
+    print(f'Loaded {len(dataset)} entries')
+
+EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+
+VECTOR_DB = []
+
+def add_chunk_to_database(chunk):
+  embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
+  VECTOR_DB.append((chunk, embedding))
